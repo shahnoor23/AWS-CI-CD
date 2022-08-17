201b5a442750ea26c8c74ab6d1b354158ec608b1
shahnoor.khalidi <shahnoor.khalidi@psw.gov.pk>
2022-08-17 06:35:10 -0700

    file added

--- my_app.py
+++ my_app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! Learn Python!"
+    return "Hello World! Learn AWS CI/CD!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
